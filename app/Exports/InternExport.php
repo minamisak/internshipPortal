b82acda014cf1b7b11a137f0e70ce974e72e3f17
Misak mina <mina.m.misak@gmail.com>
2023-05-17 08:14:05 +0300

    excel heading

--- app/Exports/InternExport.php
+++ app/Exports/InternExport.php
@@ -4,8 +4,10 @@ namespace App\Exports;
 
 use App\Models\Intern;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class InternExport implements FromCollection
+class InternExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +15,34 @@ class InternExport implements FromCollection
     public function collection()
     {
         return Intern::all();
+    }
+
+    public function headings(): array
+    {
+        // return an array of headings
+        return ['Name', 'Email','Mobile','City','Address','University','Bachelor Degree','Graduation Year','Major','Preferred Industry','Preferred Training Field','Grade','Training Info', 'Created At', 'Updated At'];
+    }
+
+    public function map($intern): array
+    {
+        // return an array of values to be mapped to columns
+        return [
+            $intern->full_name,
+            $intern->email,
+            $intern->mobile,
+            $intern->city,
+            $intern->address,
+            $intern->university,
+            $intern->bachelor_degree,
+            $intern->graduation_year,
+            $intern->major,
+            $intern->preferred_industry,
+            $intern->preferred_training_field,
+            $intern->grade,
+            $intern->training_info,
+            $intern->source,
+            $intern->created_at,
+            $intern->updated_at,
+        ];
     }
 }
