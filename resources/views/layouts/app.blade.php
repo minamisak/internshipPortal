f8066bbab436d31b7bdceb5f70cad3e6e92d739b
Misak mina <mina.m.misak@gmail.com>
2023-06-06 22:37:11 +0300

    head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- //head -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/falogo.png') }}">
